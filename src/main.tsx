17d4e5c59c29a75b567d64cdf84bcc3e54c1cf18
Rubenia M Lima <rubenia.lima2608@gmail.com>
2025-03-07 00:15:21 -0300

    Inserindo o QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.tsx'
+import React from  'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+
+    </QueryClientProvider>
+   
+  </React.StrictMode>,
 )
